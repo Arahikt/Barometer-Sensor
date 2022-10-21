d6720a9712c0d4f1dbdb410d19c6d92975284501
Arahik Torosian <arahikt@gmail.com>
2022-10-20 22:31:03 -0700

    Revert "Code clea up."
    This reverts commit df2265c823248b088ebf1ecb8b95dcd504d57e14.

--- app/src/main/java/com/example/accelerometerandlight/HomeActivity.java
+++ app/src/main/java/com/example/accelerometerandlight/HomeActivity.java
@@ -8,19 +8,21 @@ import android.os.Handler;
 
 public class HomeActivity extends AppCompatActivity {
 
-        private static int SPLASH_TIME_OUT =1000;
+        private static int SPLASH_TIME_OUT =3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         // splash screen
-        new Handler().postDelayed(() -> {
-            Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
-            startActivity(homeIntent);
-            finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
         }, SPLASH_TIME_OUT);
-
 
     }
 }
